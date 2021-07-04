2d9e65b87b01f8c32f2af21f3a35d0e16af6cc93
emerson da conceição cabral <emersoncabral202@gmail.com>
2021-07-04 13:25:46 -0300

    ajuste

---
+++ formulario/formulario.js
@@ -0,0 +1,30 @@
+//so comita no github quando aprender validação de dados
+/*
+var nome = document.getElementById("Nome")
+var email = document.getElementById("Email")
+var telefone = document.getElementById("Telefone")
+var data = document.getElementById("Data_Nascimento")
+var cidade = document.getElementById("Cidade")
+var estado = document.getElementById("Estado")
+var endereço = document.getElementById("Endereço")
+const dadoscliente = [
+    
+]
+
+function criardados(nome,email,telefone,data,cidade,estado,endereço){
+    return{
+        nome,
+        email,
+        telefone,
+        data,
+        cidade,
+        estado,
+        endereço
+    }
+}
+ 
+function dados(){
+  dadoscliente.push(criardados(nome.value,email.value,telefone.value,data.value,cidade.value,estado.value,endereço.value))
+  console.log(dadoscliente)
+}
+*/
